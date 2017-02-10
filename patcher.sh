3341d53bc5ccd602654f447bc4ce7daadd5e7907
YumeMichi <do4suki@gmail.com>
2017-02-10 18:18:45 +0800

    Add OMS patches

--- patcher.sh
+++ patcher.sh
@@ -6,6 +6,7 @@ ROM_TREE=$PWD
 PATCHER_PATH=$ROM_TREE/patcher
 
 SECURITY=$PATCHER_PATH/security
+OMS=$PATCHER_PATH/oms
 
 ### Clean up first
 
@@ -23,7 +24,17 @@ cd $ROM_TREE/hardware/libhardware
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Bluetooth
 git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/Contacts
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/ContactsCommon
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/Dialer
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/ExactCalculator
+git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Messaging
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/PhoneCommon
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/UnifiedEmail
 git clean -f -d && git reset --hard
@@ -52,3 +63,17 @@ patch -d packages/apps/Messaging      -p1 -s -N --no-backup-if-mismatch < $SECUR
 patch -d packages/apps/Messaging      -p1 -s -N --no-backup-if-mismatch < $SECURITY/Messaging1.patch
 patch -d packages/apps/Messaging      -p1 -s -N --no-backup-if-mismatch < $SECURITY/Messaging2.patch
 patch -d packages/apps/UnifiedEmail   -p1 -s -N --no-backup-if-mismatch < $SECURITY/UnifiedEmail0.patch
+
+cd $ROM_TREE
+
+### OMS patches
+
+patch -d packages/apps/Contacts           -p1 -s -N --no-backup-if-mismatch < $OMS/Contacts0.patch
+patch -d packages/apps/ContactsCommon     -p1 -s -N --no-backup-if-mismatch < $OMS/ContactsCommon0.patch
+patch -d packages/apps/Dialer             -p1 -s -N --no-backup-if-mismatch < $OMS/Dialer0.patch
+patch -d packages/apps/Dialer             -p1 -s -N --no-backup-if-mismatch < $OMS/Dialer1.patch
+patch -d packages/apps/ExactCalculator    -p1 -s -N --no-backup-if-mismatch < $OMS/ExactCalculator0.patch
+patch -d packages/apps/ExactCalculator    -p1 -s -N --no-backup-if-mismatch < $OMS/ExactCalculator1.patch
+patch -d packages/apps/PhoneCommon        -p1 -s -N --no-backup-if-mismatch < $OMS/PhoneCommon0.patch
+
+cd $ROM_TREE
--- unpatcher.sh
+++ unpatcher.sh
@@ -20,7 +20,17 @@ cd $ROM_TREE/hardware/libhardware
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Bluetooth
 git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/Contacts
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/ContactsCommon
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/Dialer
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/ExactCalculator
+git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Messaging
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/PhoneCommon
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/UnifiedEmail
 git clean -f -d && git reset --hard
