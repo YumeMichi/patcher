19c32c87a74feb7aa9fcc351a26a4fb03a64c00c
YumeMichi <do4suki@gmail.com>
2017-02-12 14:36:32 +0000

    Add 2017-02-05 security patches

--- unpatcher.sh
+++ unpatcher.sh
@@ -5,3 +5,24 @@ ROM_TREE=$PWD
 # Clean up
 
 cd $ROM_TREE
+
+cd $ROM_TREE/build
+git clean -f -d && git reset --hard
+cd $ROM_TREE/libcore
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/av
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/base
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/native
+git clean -f -d && git reset --hard
+cd $ROM_TREE/hardware/libhardware
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/Bluetooth
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/Messaging
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/UnifiedEmail
+git clean -f -d && git reset --hard
+
+cd $ROM_TREE
