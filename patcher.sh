b566b6d63c8962ff64078e80f6cc8e7964073bf9
YumeMichi <do4suki@gmail.com>
2017-06-02 22:37:02 +0000

    Revert "Add Snap patches"
    This reverts commit 2b9a573e3684273bcf899e3d25117d6b542360f4.
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- patcher.sh
+++ patcher.sh
@@ -41,8 +41,6 @@ cd $ROM_TREE/packages/apps/PackageInstaller
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/PhoneCommon
 git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/Snap
-git clean -f -d && git reset --hard
 cd $ROM_TREE/vendor/cm
 git clean -f -d && git reset --hard
 
@@ -70,8 +68,6 @@ patch -d packages/apps/Dialer             -p1 -s -N --no-backup-if-mismatch < $O
 patch -d packages/apps/Eleven             -p1 -s -N --no-backup-if-mismatch < $OPTS/Eleven0.patch
 patch -d packages/apps/Gallery2           -p1 -s -N --no-backup-if-mismatch < $OPTS/Gallery20.patch
 patch -d packages/apps/LockClock          -p1 -s -N --no-backup-if-mismatch < $OPTS/LockClock0.patch
-patch -d packages/apps/Snap               -p1 -s -N --no-backup-if-mismatch < $OPTS/Snap0.patch
-patch -d packages/apps/Snap               -p1 -s -N --no-backup-if-mismatch < $OPTS/Snap1.patch
 patch -d vendor/cm                        -p1 -s -N --no-backup-if-mismatch < $OPTS/vendor-cm0.patch
 
 ### OMS patches
