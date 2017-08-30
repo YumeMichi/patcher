f97290276cde46596b3b059aa2f2b63b84973102
YumeMichi <do4suki@gmail.com>
2017-08-30 23:23:36 +0000

    Add Contacts icon patch
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- patcher.sh
+++ patcher.sh
@@ -67,6 +67,7 @@ patch -d packages/apps/CMParts            -p1 -s -N --no-backup-if-mismatch < $O
 patch -d packages/apps/CMUpdater          -p1 -s -N --no-backup-if-mismatch < $OPTS/CMUpdater0.patch
 patch -d packages/apps/CMUpdater          -p1 -s -N --no-backup-if-mismatch < $OPTS/CMUpdater1.patch
 patch -d packages/apps/CMUpdater          -p1 -s -N --no-backup-if-mismatch < $OPTS/CMUpdater2.patch
+patch -d packages/apps/Contacts           -p1 -s -N --no-backup-if-mismatch < $OPTS/Contacts1.patch
 patch -d packages/apps/Dialer             -p1 -s -N --no-backup-if-mismatch < $OPTS/Dialer2.patch
 patch -d packages/apps/Dialer             -p1 -s -N --no-backup-if-mismatch < $OPTS/Dialer3.patch
 patch -d packages/apps/Eleven             -p1 -s -N --no-backup-if-mismatch < $OPTS/Eleven0.patch
