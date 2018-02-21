e2a79a271f961ce97512ade64cb5e535ba971817
YumeMichi <do4suki@gmail.com>
2018-02-21 23:40:13 +0800

    Add external/littlemock patches
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- unpatcher.sh
+++ unpatcher.sh
@@ -8,6 +8,8 @@ cd $ROM_TREE/build/make
 git clean -f -d && git reset --hard
 cd $ROM_TREE/device/qcom/sepolicy
 git clean -f -d && git reset --hard
+cd $ROM_TREE/external/littlemock
+git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Updater
