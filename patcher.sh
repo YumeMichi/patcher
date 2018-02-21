a2a4256a477ce33361f3f699cce7e1ff0fc40bb3
YumeMichi <do4suki@gmail.com>
2018-02-21 23:13:19 +0800

    Add vendor/lineage patches
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- patcher.sh
+++ patcher.sh
@@ -9,8 +9,18 @@ PATCHES=$PATCHER_PATH/patches
 
 ### Clean up first
 
+cd $ROM_TREE/vendor/lineage
+git clean -f -d && git reset --hard
+
 cd $ROM_TREE
 
 ### Patches
+
+patch -d vendor/lineage                   -p1 -s -N --no-backup-if-mismatch < $PATCHES/vendor-lineage-01.patch
+patch -d vendor/lineage                   -p1 -s -N --no-backup-if-mismatch < $PATCHES/vendor-lineage-02.patch
+patch -d vendor/lineage                   -p1 -s -N --no-backup-if-mismatch < $PATCHES/vendor-lineage-03.patch
+patch -d vendor/lineage                   -p1 -s -N --no-backup-if-mismatch < $PATCHES/vendor-lineage-04.patch
+patch -d vendor/lineage                   -p1 -s -N --no-backup-if-mismatch < $PATCHES/vendor-lineage-05.patch
+patch -d vendor/lineage                   -p1 -s -N --no-backup-if-mismatch < $PATCHES/vendor-lineage-06.patch
 
 cd $ROM_TREE
--- unpatcher.sh
+++ unpatcher.sh
@@ -4,8 +4,7 @@ ROM_TREE=$PWD
 
 # Clean up
 
-cd $ROM_TREE
-
-
+cd $ROM_TREE/vendor/lineage
+git clean -f -d && git reset --hard
 
 cd $ROM_TREE
