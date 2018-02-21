e255bc6a77b88bd1bf31a7ad5fa3a4ab9d75538b
YumeMichi <do4suki@gmail.com>
2018-02-21 23:18:30 +0800

    Add packages/apps/Updater patches
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- patcher.sh
+++ patcher.sh
@@ -9,6 +9,8 @@ PATCHES=$PATCHER_PATH/patches
 
 ### Clean up first
 
+cd $ROM_TREE/packages/apps/Updater
+git clean -f -d && git reset --hard
 cd $ROM_TREE/vendor/lineage
 git clean -f -d && git reset --hard
 
@@ -16,6 +18,8 @@ cd $ROM_TREE
 
 ### Patches
 
+patch -d packages/apps/Updater            -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Updater-01.patch
+patch -d packages/apps/Updater            -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Updater-02.patch
 patch -d vendor/lineage                   -p1 -s -N --no-backup-if-mismatch < $PATCHES/vendor-lineage-01.patch
 patch -d vendor/lineage                   -p1 -s -N --no-backup-if-mismatch < $PATCHES/vendor-lineage-02.patch
 patch -d vendor/lineage                   -p1 -s -N --no-backup-if-mismatch < $PATCHES/vendor-lineage-03.patch
--- unpatcher.sh
+++ unpatcher.sh
@@ -4,6 +4,8 @@ ROM_TREE=$PWD
 
 # Clean up
 
+cd $ROM_TREE/packages/apps/Updater
+git clean -f -d && git reset --hard
 cd $ROM_TREE/vendor/lineage
 git clean -f -d && git reset --hard
 
