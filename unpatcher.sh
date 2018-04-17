3cc5b03d8556e1cfe451d4006fa11b60ab8028c3
YumeMichi <do4suki@gmail.com>
2018-04-17 12:18:23 +0800

    Add f2fs assert patch
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- unpatcher.sh
+++ unpatcher.sh
@@ -5,6 +5,8 @@ ROM_TREE=$PWD
 # Clean up
 cd $ROM_TREE/build
 git clean -f -d && git reset --hard
+cd $ROM_TREE/device/oppo/common
+git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/av
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
