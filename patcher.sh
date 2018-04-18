d4012eff7a6bb32c46be5314c797dbafe463c13a
YumeMichi <do4suki@gmail.com>
2018-04-18 10:37:38 +0800

    Cleanup patcher
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- patcher.sh
+++ patcher.sh
@@ -5,7 +5,7 @@
 ROM_TREE=$PWD
 PATCHER_PATH=$ROM_TREE/patcher
 
-PATCHES=$PATCHER_PATH/patches
+PATCHES=$PATCHER_PATH/YumeMichi
 
 ### Clean up first
 
@@ -28,24 +28,24 @@ cd $ROM_TREE
 
 ### Patches
 
-patch -d build/make                       -p1 -s -N --no-backup-if-mismatch < $PATCHES/build-make-01.patch
-patch -d build/make                       -p1 -s -N --no-backup-if-mismatch < $PATCHES/build-make-02.patch
-patch -d build/make                       -p1 -s -N --no-backup-if-mismatch < $PATCHES/build-make-03.patch
-patch -d build/make                       -p1 -s -N --no-backup-if-mismatch < $PATCHES/build-make-04.patch
-patch -d device/oppo/common               -p1 -s -N --no-backup-if-mismatch < $PATCHES/oppo-common-01.patch
-patch -d hardware/interfaces              -p1 -s -N --no-backup-if-mismatch < $PATCHES/hardware-interfaces-01.patch
-patch -d hardware/qcom/display-caf/msm8974 -p1 -s -N --no-backup-if-mismatch < $PATCHES/hardware-qcom-display-01.patch
-patch -d packages/apps/Settings           -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-01.patch
-patch -d packages/apps/Settings           -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-02.patch
-patch -d packages/apps/Settings           -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-03.patch
-patch -d packages/apps/Settings           -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-04.patch
-patch -d packages/apps/Settings           -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-05.patch
-patch -d packages/apps/Settings           -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-06.patch
-patch -d packages/apps/Settings           -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-07.patch
-patch -d packages/apps/Settings           -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-08.patch
-patch -d packages/apps/Updater            -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Updater-01.patch
-patch -d packages/apps/Updater            -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Updater-02.patch
-patch -d system/core                      -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-core-01.patch
-patch -d system/core                      -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-core-02.patch
+patch -d build/make                           -p1 -s -N --no-backup-if-mismatch < $PATCHES/build-make-01.patch
+patch -d build/make                           -p1 -s -N --no-backup-if-mismatch < $PATCHES/build-make-02.patch
+patch -d build/make                           -p1 -s -N --no-backup-if-mismatch < $PATCHES/build-make-03.patch
+patch -d build/make                           -p1 -s -N --no-backup-if-mismatch < $PATCHES/build-make-04.patch
+patch -d device/oppo/common                   -p1 -s -N --no-backup-if-mismatch < $PATCHES/oppo-common-01.patch
+patch -d hardware/interfaces                  -p1 -s -N --no-backup-if-mismatch < $PATCHES/hardware-interfaces-01.patch
+patch -d hardware/qcom/display-caf/msm8974    -p1 -s -N --no-backup-if-mismatch < $PATCHES/hardware-qcom-display-01.patch
+patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-01.patch
+patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-02.patch
+patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-03.patch
+patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-04.patch
+patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-05.patch
+patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-06.patch
+patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-07.patch
+patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-08.patch
+patch -d packages/apps/Updater                -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Updater-01.patch
+patch -d packages/apps/Updater                -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Updater-02.patch
+patch -d system/core                          -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-core-01.patch
+patch -d system/core                          -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-core-02.patch
 
 cd $ROM_TREE
