3c91473c2fac36609a4fbb086715ff8763bb947c
YumeMichi <do4suki@gmail.com>
2018-07-25 14:05:36 +0800

    Cleanup unused patches
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- patcher.sh
+++ patcher.sh
@@ -11,15 +11,11 @@ PATCHES=$PATCHER_PATH/YumeMichi
 
 cd $ROM_TREE/build/make
 git clean -f -d && git reset --hard
-cd $ROM_TREE/hardware/interfaces
-git clean -f -d && git reset --hard
 cd $ROM_TREE/hardware/qcom/display-caf/msm8974
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/Updater
 git clean -f -d && git reset --hard
 cd $ROM_TREE/system/core
 git clean -f -d && git reset --hard
@@ -31,7 +27,6 @@ cd $ROM_TREE
 ### Patches
 
 patch -d build/make                           -p1 -s -N --no-backup-if-mismatch < $PATCHES/build-make-01.patch
-patch -d hardware/interfaces                  -p1 -s -N --no-backup-if-mismatch < $PATCHES/hardware-interfaces-01.patch
 patch -d hardware/qcom/display-caf/msm8974    -p1 -s -N --no-backup-if-mismatch < $PATCHES/hardware-qcom-display-01.patch
 patch -d packages/apps/LockClock              -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-LockClock-01.patch
 patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-01.patch
@@ -39,12 +34,6 @@ patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch 
 patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-03.patch
 patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-04.patch
 patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-05.patch
-patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-06.patch
-patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-07.patch
-patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-08.patch
-patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-09.patch
-patch -d packages/apps/Updater                -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Updater-01.patch
-patch -d packages/apps/Updater                -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Updater-02.patch
 patch -d system/core                          -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-core-01.patch
 patch -d system/core                          -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-core-02.patch
 patch -d system/sepolicy                      -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-sepolicy-01.patch
--- unpatcher.sh
+++ unpatcher.sh
@@ -6,15 +6,11 @@ ROM_TREE=$PWD
 
 cd $ROM_TREE/build/make
 git clean -f -d && git reset --hard
-cd $ROM_TREE/hardware/interfaces
-git clean -f -d && git reset --hard
 cd $ROM_TREE/hardware/qcom/display-caf/msm8974
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/Updater
 git clean -f -d && git reset --hard
 cd $ROM_TREE/system/core
 git clean -f -d && git reset --hard
