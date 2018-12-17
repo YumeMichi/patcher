8251243f147ee84e940adc1cedb0bee654376263
YumeMichi <do4suki@gmail.com>
2018-12-17 16:34:05 +0800

    Rearrange patcher
    Change-Id: Ib199a2db987790f92b38ebfd48634850d14b29b6
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- patcher.sh
+++ patcher.sh
@@ -1,41 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-### Apply these patches before compilation:
+ROOT="${PWD}"
+REPOSITORIES=(
+    'build/make'
+    'packages/apps/LockClock'
+    'packages/apps/Settings'
+    'system/core'
+    'system/sepolicy'
+)
 
-ROM_TREE=$PWD
-PATCHER_PATH=$ROM_TREE/patcher
+for repository in "${REPOSITORIES[@]}"; do
+    cd "${ROOT}/${repository}"
 
-PATCHES=$PATCHER_PATH/YumeMichi
+    git am --keep-cr "${ROOT}/patcher/YumeMichi/${repository}"/*
 
-### Clean up first
-
-cd $ROM_TREE/build/make
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/LockClock
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/Settings
-git clean -f -d && git reset --hard
-cd $ROM_TREE/system/core
-git clean -f -d && git reset --hard
-cd $ROM_TREE/system/sepolicy
-git clean -f -d && git reset --hard
-
-cd $ROM_TREE
-
-### Patches
-
-patch -d build/make                           -p1 -s -N --no-backup-if-mismatch < $PATCHES/build-make-01.patch
-patch -d packages/apps/LockClock              -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-LockClock-01.patch
-patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-01.patch
-patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-02.patch
-patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-03.patch
-patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-04.patch
-patch -d packages/apps/Settings               -p1 -s -N --no-backup-if-mismatch < $PATCHES/apps-Settings-05.patch
-patch -d system/core                          -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-core-01.patch
-patch -d system/core                          -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-core-02.patch
-patch -d system/core                          -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-core-03.patch
-patch -d system/sepolicy                      -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-sepolicy-01.patch
-patch -d system/sepolicy                      -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-sepolicy-02.patch
-patch -d system/sepolicy                      -p1 -s -N --no-backup-if-mismatch < $PATCHES/system-sepolicy-03.patch
-
-cd $ROM_TREE
+    cd "${ROOT}"
+done
