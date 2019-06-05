096ff50aa36f0f569b348e3b163a3dccd16cb603
Michael Ortner <lineage@michael-ortner.de>
2019-06-05 21:22:17 +0200

    Adds mishbieg folder to patcher and adds reverter script
    Change-Id: I02e3176f890ca95b5939f83fe3ee5aa4ea45de8a

--- reverter.sh
+++ reverter.sh
@@ -10,19 +10,19 @@ REPOSITORIES=(
 	'vendor/lineage'
 )
 
-echo Applying patches from patcher
+echo Reverting patches from patcher
 for repository in "${REPOSITORIES[@]}"; do
     cd "${ROOT}/${repository}"
 
 	echo
 	echo "In directory: $PWD"
-	if [ -d "${ROOT}/patcher/YumeMichi/${repository}" ]; then
-		git am --keep-cr ${ROOT}/patcher/YumeMichi/${repository}/*.patch
-	fi
-
-	if [ -d "${ROOT}/patcher/mishbieg/${repository}" ]; then
-		git am --keep-cr ${ROOT}/patcher/mishbieg/${repository}/*.patch
-	fi
+	echo Aborting am
+	git am --abort
+	git reset --hard
+	echo Checking out remotes/github/lineage-16.0
+	git checkout remotes/github/lineage-16.0
+	echo Cleaning up repo
+	git clean -fd
 
     cd "${ROOT}"
 done
