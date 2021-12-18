8fb14d5231caf4659edc6d4baad1a7cc2ccf5b9c
YumeMichi <do4suki@gmail.com>
2021-12-18 22:34:09 +0800

    patcher: Initial empty patcher
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: Iad9de3f7df3ab3d8e5f576e5a86ffb7d458cfae7

--- patcher.sh
+++ patcher.sh
@@ -2,21 +2,12 @@
 
 ROOT="${PWD}"
 REPOSITORIES=(
-    'bionic'
-    'build/soong'
-    'device/lineage/sepolicy'
-    'device/qcom/sepolicy-legacy'
-    'frameworks/av'
-    'packages/apps/Snap'
-    'system/core'
-    'system/sepolicy'
-    'vendor/lineage'
 )
 
 for repository in "${REPOSITORIES[@]}"; do
     cd "${ROOT}/${repository}"
 
-    git am --keep-cr "${ROOT}/patcher/YumeMichi/${repository}"/*
+    git am --keep-cr "${ROOT}/patcher/aospa/${repository}"/*
 
     cd "${ROOT}"
 done
