7e27e03931413843cf27dc9a64e22232a3740989
YumeMichi <do4suki@gmail.com>
2021-12-24 18:23:13 +0800

    patcher: Add 2020-11 ASB
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: I21db3804e2cd2ff0375f72d84a24e2551c24eacb

--- patcher.sh
+++ patcher.sh
@@ -4,14 +4,23 @@ ROOT="${PWD}"
 REPOSITORIES=(
     'bionic'
     'build/make'
+    'external/aac'
+    'external/libexif'
+    'frameworks/base'
+    'frameworks/native'
     'hardware/pa/interfaces'
     'hardware/qcom/wlan'
     'hardware/ril'
+    'packages/apps/Bluetooth'
+    'packages/apps/CellBroadcastReceiver'
     'packages/apps/Snap'
     'packages/services/Telecomm'
     'packages/services/Telephony'
     'system/bt'
+    'system/nfc'
+    'system/netd'
     'vendor/pa'
+    'vendor/qcom/opensource/commonsys/packages/apps/Bluetooth'
     'vendor/qcom/opensource/commonsys/system/bt'
 )
 
