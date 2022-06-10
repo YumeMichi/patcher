dab4a45e4915d23601202622c951f6c749933609
Yuan Si <do4suki@gmail.com>
2022-06-11 01:05:16 +0800

    patcher: Add 2022-06 ASB
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: I878503d7c485e77113f01083a54704e74445f630

--- patcher.sh
+++ patcher.sh
@@ -2,6 +2,19 @@
 
 ROOT="${PWD}"
 REPOSITORIES=(
+    'external/aac'
+    'frameworks/base'
+    # 'packages/apps/Bluetooth'
+    'packages/apps/Contacts'
+    'packages/apps/Dialer'
+    'packages/apps/EmergencyInfo'
+    'packages/apps/ManagedProvisioning'
+    # 'packages/apps/Nfc'
+    'packages/apps/Settings'
+    'packages/services/Telecomm'
+    # 'system/bt'
+    # 'system/nfc'
+    'vendor/nxp/opensource/commonsys/external/libnfc-nci'
     'vendor/nxp/opensource/commonsys/packages/apps/Nfc'
     'vendor/qcom/opensource/commonsys/system/bt'
 )
