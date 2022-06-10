0662073a13fbf66b213f10a1636543f8f679c504
Yuan Si <do4suki@gmail.com>
2022-06-11 00:39:46 +0800

    patcher: Add missing 2022-04 ASB
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: Ib0a8184f2ac381d746d61f0139cf856dbdb80ac2

--- patcher.sh
+++ patcher.sh
@@ -2,6 +2,8 @@
 
 ROOT="${PWD}"
 REPOSITORIES=(
+    'vendor/nxp/opensource/commonsys/packages/apps/Nfc'
+    'vendor/qcom/opensource/commonsys/system/bt'
 )
 
 for repository in "${REPOSITORIES[@]}"; do
